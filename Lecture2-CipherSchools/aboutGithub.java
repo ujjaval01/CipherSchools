129d9361ff7c73962c08b6f19e60ce863281ea3a
ujjaval01 <sainiujvl@gmail.com>
2024-07-25 11:28:37 +0530

    adding git commands

--- Lecture2-CipherSchools/aboutGithub.java
+++ Lecture2-CipherSchools/aboutGithub.java
@@ -1,5 +1,27 @@
 public class aboutGithub {
     public static void main(String[] args) {
         System.out.println("Github");
+
+        /*
+         git init
+git add
+git log
+git remote add origin https
+git remote -v
+gir push origin master/main
+git clone https
+--------------------------------
+
+git init
+git status
+git add .
+git commit -m "first ccommit"
+git remote add origin https
+git branch
+git branch -m master main   (master to main)
+git push origin main
+
+         */
+
     }
 }
