036d8ec61aa39fe6bd7eb6e732bb9a3e98edc109
ujjaval01 <sainiujvl@gmail.com>
2024-07-26 15:50:41 +0530

    first commit

--- Lecture4-CipherSchools/aboutDataTypes.java
+++ Lecture4-CipherSchools/aboutDataTypes.java
@@ -1,3 +1,4 @@
+
 public class aboutDataTypes{
     public static void main(String[] args){
         // System.out.println("hello");
---
+++ Lecture5-CipherSchools/Operators.java
@@ -0,0 +1,15 @@
+  public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Example of Operators");
+
+        //increment and decrement Operators
+
+        int a = 5;
+        a++;   //increment
+        a--;   //decrement
+        System.out.println("Original: "+ a);
+        System.out.println("Increment: " + a++);
+        System.out.println("Decrement: "+ a--);
+    }
+    
+}
